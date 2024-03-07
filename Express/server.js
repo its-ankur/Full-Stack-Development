24711d2e9d6c3fbfba4cd8866bf3284952edf5e3
its-ankur <ankuratul2007@gmail.com>
2024-03-07 14:35:49 +0530

    Practice

--- Express/server.js
+++ Express/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => {
+const path = require('path');
+app.use(express.static('.'));
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, './index.html'));
+    res.send('Hello World');
+});
+app.post('/getData', (req, res) => {
+    res.send('Welcome '+req.body.name+'!');
+});
+
+app.listen(3001, () => {
   console.log('server started');
 });
