397143c38a68d5453c096684c1ef25b8b58c44a8
its-ankur <ankuratul2007@gmail.com>
2024-03-08 19:47:06 +0530

    used fetch

--- Express/todo/server.js
+++ Express/todo/server.js
@@ -1,38 +1,48 @@
-const express=require('express');
-const fs=require('fs');
-const app=express();
-app.use(express.static('.'));
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.static("."));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/index.html");
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-app.post("/addData",(req,res)=>{
-    console.log(req.body);
+app.post("/addData", (req, res) => {
+  console.log(req.body);
 
-    // let data;
-    try{
-        data=fs.readFileSync("todo.txt","utf8");
-        data=JSON.parse(data);
-    }
-    catch(err){
-        data=[];
-    }
-    data.push(req.body);
-    fs.writeFileSync("todo.txt",JSON.stringify(data));
-    res.status(200).json(req.body);
+  // let data;
+  try {
+    data = fs.readFileSync("todo.txt", "utf8");
+    data = JSON.parse(data);
+  } catch (err) {
+    data = [];
+  }
+  data.push(req.body);
+  fs.writeFileSync("todo.txt", JSON.stringify(data));
+  res.status(200).json(req.body);
 });
-app.get('/showData',(req,res)=>{
-    let data;
-    try{
-        data=fs.readFileSync("todo.txt","utf8");
-        data=JSON.parse(data);
-    }
-    catch(err){
-        data=[];
-    }
-    res.status(200).json(req.body);
+app.get("/showData", (req, res) => {
+  let data;
+  try {
+    data = fs.readFileSync("todo.txt", "utf8");
+    data = JSON.parse(data);
+  } catch (err) {
+    data = [];
+  }
+  res.status(200).json(req.body);
 });
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.delete("/deleteData", (req, res) => {
+  let data;
+  try {
+    data = fs.readFileSync("todo.txt", "utf8");
+    data = JSON.parse(data);
+  } catch (err) {
+    data = [];
+  }
+  data.splice(req.query.index, 1);
+  fs.writeFileSync("todo.txt", JSON.stringify(data));
+  res.status(200).json(req.body);
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
