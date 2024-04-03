00a1ff46642c76a418ff92214acd08a6ce614891
its-ankur <ankuratul2007@gmail.com>
2024-04-03 12:21:33 +0530

    not any specific changes

--- mongoCompass/server.js
+++ mongoCompass/server.js
@@ -3,25 +3,25 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 mongoose.connect("mongodb://127.0.0.1:27017/test").then(() => {
   console.log("Connected to database!");
+}).catch((error) => {
+  console.error("Error connecting to database:", error);
 });
-
-let schema = new mongoose.Schema({
-  name: String,
-},{
-    versionKey: false
-});
-
+let schema = new mongoose.Schema(
+  {
+    name: String,
+  },
+  {
+    versionKey: false,
+  }
+);
 let Model = mongoose.model("Model", schema);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
 app.post("/insert", async (req, res) => {
   console.log(req.body);
   let newDocument = new Model(req.body);
@@ -34,7 +34,6 @@ app.post("/insert", async (req, res) => {
     res.status(500).json({ error: "Error saving data" });
   }
 });
-
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
