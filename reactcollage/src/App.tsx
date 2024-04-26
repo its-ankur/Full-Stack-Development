729f686b724f5bb2f15a5e8aaec4c83f1d8007e0
its-ankur <ankuratul2007@gmail.com>
2024-04-26 10:52:57 +0530

    state management and css added

--- reactcollage/src/App.tsx
+++ reactcollage/src/App.tsx
@@ -6,25 +6,35 @@ import GreetClass from "./GreetClass";
 import Counter from "./Counter";
 import CounterClass from "./CounterClass";
 import CTimer from "./CTimer";
+import ListGroup from "./ListGroup";
+import UI from "./UI";
 
-const ClickHandler1 = () => {
-  console.log("Click handler 1");
-};
-const ClickHandler2 = () => {
-  console.log("Click handler 2");
-}
-function App() {
+// const ClickHandler1 = () => {
+//   console.log("Click handler 1");
+// };
+// const ClickHandler2 = () => {
+//   console.log("Click handler 2");
+// }
+// function App() {
+//   return (
+//     <div>
+//       {/* <CTimer initial={50}/> */}
+//       {/* <Greet myClick={ClickHandler1} value="Code" data={10} />
+//       <Greet myClick={ClickHandler2} value="React" data={20} />
+//       <Greet value="TypeScript" data={30} /> */}
+//       {/* <GreetClass value="Class"/> */}
+//       {/* <Counter/> */}
+//       <CounterClass/>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
-    <div>
-      {/* <CTimer initial={50}/> */}
-      {/* <Greet myClick={ClickHandler1} value="Code" data={10} />
-      <Greet myClick={ClickHandler2} value="React" data={20} />
-      <Greet value="TypeScript" data={30} /> */}
-      {/* <GreetClass value="Class"/> */}
-      {/* <Counter/> */}
-      <CounterClass/>
-    </div>
-  );
+    // <ListGroup/>
+    <UI/>
+  )
 }
+
 
 export default App;
